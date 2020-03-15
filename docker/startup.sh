7d87d3d3f704c3613f8090af5f7d80dc8e94e9a3
MichaelMorris <michael.morris@est.tech>
2020-03-15 17:02:11 +0000

    Run pods as non-root user
    Change-Id: I443a031df5e72aed5865fb21051bec1bcd823699
    Issue-ID: SDC-2798
    Signed-off-by: MichaelMorris <michael.morris@est.tech>

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,7 +4,7 @@ JAVA_OPTIONS=" ${JAVA_OPTIONS} -Dconfig.home=${JETTY_BASE}/config
                                -Dlog.home=${JETTY_BASE}/logs
                                -Dlogback.configurationFile=${JETTY_BASE}/dcae-fe/logback-spring.xml"
 
-cd /root/chef-solo
+cd /var/lib/jetty/chef-solo
 chef-solo -c solo.rb -E ${ENVNAME}
 
 status=$?
